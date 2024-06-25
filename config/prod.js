3a56d2565446f845c7abaf14f82e11b72cd1151f
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-06-25 12:16:07 +0300

    Change prod

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,8 @@
 export default {
     // dbURL: 'mongodb+srv://amir:amir123@cluster0.a5p0yzs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     // dbURL: 'mongodb+srv://amir:W5uAmR0PLcYdRVmz@cluster0.a5p0yzs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    dbURL: 'mongodb+srv://amir:PiElWkxVUTl9UdQK@cluster0.a5p0yzs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    // dbURL: 'mongodb+srv://amir:PiElWkxVUTl9UdQK@cluster0.a5p0yzs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    dbURL: `mongodb+srv://amir:${process.env.SECRETMONGO}@cluster0.a5p0yzs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
     // dbURL: `mongodb+srv://amir:${process.env.SECRETMONGO}@cluster0.a5p0yzs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
     dbName: 'comfy-store-db',
 }
